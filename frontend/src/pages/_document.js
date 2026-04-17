0f0ca4411f558a7c8683b90172a19a5b552a80d8
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2026-04-17 16:00:02 +0200

    add favicon

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -7,6 +7,8 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#0a5a8c" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
